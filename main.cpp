7a92fc251b9c36f5feed455f03a796ea3bcaf94e
MelvinSu <32498328+MelvinSu@users.noreply.github.com>
2021-01-16 15:29:49 -0800

    button test
    button test

--- main.cpp
+++ main.cpp
@@ -1,11 +1,22 @@
 #include "mainwindow.h"
 
 #include <QApplication>
+#include <QPushButton>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
+
+    QPushButton button;
+    button.setText("Text Inside Button");
+    button.setToolTip("Hover over text");
+    button.show();
+
+    QFont font("Courier");
+    button.setFont(font);
+
+    QIcon icon ("C:/Windows/System32/Snooze_80.png");
+    button.setIcon(icon);
+
     return a.exec();
 }
