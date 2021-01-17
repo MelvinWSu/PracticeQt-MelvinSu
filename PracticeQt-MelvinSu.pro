d9788862d1321d9a8197d6abc2063a34b0de21df
MelvinSu <32498328+MelvinSu@users.noreply.github.com>
2021-01-16 16:34:11 -0800

    subclass/inheritance example

--- PracticeQt-MelvinSu.pro
+++ PracticeQt-MelvinSu.pro
@@ -10,10 +10,12 @@ CONFIG += c++11
 
 SOURCES += \
     main.cpp \
-    mainwindow.cpp
+    mainwindow.cpp \
+    window.cpp
 
 HEADERS += \
-    mainwindow.h
+    mainwindow.h \
+    window.h
 
 FORMS += \
     mainwindow.ui
--- main.cpp
+++ main.cpp
@@ -1,22 +1,15 @@
 #include "mainwindow.h"
+#include "window.h"
 
 #include <QApplication>
 #include <QPushButton>
 
 int main(int argc, char *argv[])
 {
-    QApplication a(argc, argv);
+    QApplication a (argc, argv);
 
-    QPushButton button;
-    button.setText("Text Inside Button");
-    button.setToolTip("Hover over text");
-    button.show();
-
-    QFont font("Courier");
-    button.setFont(font);
-
-    QIcon icon ("C:/Windows/System32/Snooze_80.png");
-    button.setIcon(icon);
+    Window window;
+    window.show();
 
     return a.exec();
 }
